17f74d99224f146f745ae0a115a7af1e7de1e2be
Venkat Sundaram <66926820+vsundaram23@users.noreply.github.com>
2025-01-28 18:01:23 -0600

    removed static folder in client

--- client/src/config.js
+++ client/src/config.js
@@ -8,8 +8,8 @@ console.log('Type of NODE_ENV:', typeof env);
 
 const BACKEND_URL = 'https://maven-backend-x94s.onrender.com'
 
-console.log('Environment in config:', process.env.NODE_ENV);
-console.log('BACKEND_URL in config:', BACKEND_URL);
+// console.log('Environment in config:', process.env.NODE_ENV);
+// console.log('BACKEND_URL in config:', BACKEND_URL);
 
 console.log('Version in config: 1.1');
 export default BACKEND_URL;
