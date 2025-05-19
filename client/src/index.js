e4b3f319e706a7e1ef5edd32eb49ab71b36274f1
smahendra14 <satvik.mahendra@gmail.com>
2025-05-19 16:09:21 -0500

    update clerk key

--- client/src/index.js
+++ client/src/index.js
@@ -5,14 +5,7 @@ import './styles/global.css';
 import './index.css';
 import { ClerkProvider } from '@clerk/clerk-react';
 
-
-const test_value = process.env.REACT_APP_TEST_VALUE;
-
-if (!test_value) {
-  throw new Error('Add your test value to the .env file');
-}
-
-const PUBLISHABLE_KEY = process.env.REACT_APP_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = 'pk_live_Y2xlcmsudHJpZWRhbmR0cnVzdGVkLmFpJA';
 
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file');
