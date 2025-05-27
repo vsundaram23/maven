33c449140ebe3b396fb83f1d1e734c6b2e293b0e
Venkat Sundaram <66926820+vsundaram23@users.noreply.github.com>
2025-05-26 20:03:56 -0700

    fixed community issue

--- client/src/pages/CommunityProfile/CommunityProfile.js
+++ client/src/pages/CommunityProfile/CommunityProfile.js
@@ -5,8 +5,8 @@ import { FaUserTie, FaCalendarAlt, FaUsers, FaStar, FaEdit, FaSignInAlt, FaUserP
 import QuoteModal from "../../components/QuoteModal/QuoteModal";
 import "./CommunityProfile.css";
 
-// const API_URL = 'https://api.seanag-recommendations.org:8080';
-const API_URL = "http://localhost:3000";
+const API_URL = 'https://api.seanag-recommendations.org:8080';
+// const API_URL = "http://localhost:3000";
 
 const IconText = ({ icon, text, className = "" }) => (
   <div className={`icon-text-item ${className}`}> {icon} <span>{text}</span> </div>
